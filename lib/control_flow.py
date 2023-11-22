650b28c01dd40dd02297936634a5af2eb440f5e9
bill <bill.arnold@student.moringaschool.com>
2023-11-22 21:35:13 +0300

    complete

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    if username == "admin"or "ADMIN":
-        password="12345"
+    if username in ["admin", "ADMIN"] and password == "12345":
         return "Access granted"
     else:
         return "Access denied"
+
+
     
 print (admin_login("sudo", "12345"))
 # "Access denied"
@@ -16,13 +16,18 @@ print (admin_login("ADMIN", "12345"))
 # "Access granted"
 
 def hows_the_weather(temperature):
-   if temperature < 40:
-    return "It's brisk out there!"
-   elif temperature >= 40 and temperature <=65:
-    return "It's a little chilly out there!"
-   elif temperature > 85:
-    return "It's perfect out there!"
-   pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif 40 <= temperature < 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
+
+# Now your test cases should pass with this updated function.
+
+
    
 print(hows_the_weather(33))
 
